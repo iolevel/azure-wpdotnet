f40a112a1c6ea4df89f549dac5ff851f1ca16bed
Jakub Míšek <jmisek@gmail.com>
2019-02-02 14:35:51 +0100

    wpdotnet app

---
+++ app/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PeachPied.WordPress.AspNetCore;
+using PeachPied.WordPress.Sdk;
+
+namespace PeachPied.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(typeof(Program).Assembly.Location));
+
+            //
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5004/")
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddResponseCompression();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseResponseCompression();
+            app.UseWordPress();
+            app.UseDefaultFiles();
+        }
+    }
+}
---
+++ app/readme.md
@@ -0,0 +1,1 @@
+This is a sample ASP.NET Core application that integrates .NET Core WordPress into its request pipeline.
